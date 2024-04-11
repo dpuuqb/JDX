1fe0c6c7a653e0c1037fd93eb833bd5a2edd21e9
zhenyu.yin20 <zhenyu.yin20@gmail.com>
2024-04-10 23:54:55 -0400

    add resources

---
+++ Js/jms_DelegationRibbon.js
@@ -0,0 +1,37 @@
+var DelegationRibbon= (function (DelegationRibbon) {
+
+	DelegationRibbon.Publish = (primaryControl)=>{
+		console.log("Publish");
+        //open progress bar
+        Xrm.Utility.showProgressIndicator('Processing...')
+        //input validation
+        //update status if possible
+       
+        primaryControl.getAttribute('statuscode').setValue(952700000);
+        primaryControl.data.save().then(()=>Xrm.Utility.closeProgressIndicator());
+        
+	}
+    DelegationRibbon.EnablePublish = (primaryControl)=>{
+        //customize enable rule for Publish
+        //update mode && status is draft
+        return primaryControl.ui.getFormType() != 1 && primaryControl.getAttribute('statuscode').getValue() == 1
+    }
+
+	DelegationRibbon.Cancel= (primaryControl)=> {
+		console.log("Canceled");
+        var formContext = primaryControl || Xrm.Page;
+
+        Xrm.WebApi.updateRecord('jms_delegation', primaryControl.data.getEntity().getId(), {'statecode':1,'statuscode':2})
+        .then((result)=>primaryControl.data.refresh(), (error)=>Xrm.Navigation.openErrorDialog({message:error.message}))
+	}
+    
+    DelegationRibbon.EnableCancel = (primaryControl)=>{
+        //customize enable rule for Cancel
+        //update mode && status is pending or delegating
+        return primaryControl.ui.getFormType() != 1 && (primaryControl.getAttribute('statuscode').getValue() == 952700001 || primaryControl.getAttribute('statuscode').getValue() == 952700002)
+    }
+
+	
+
+	return DelegationRibbon;
+}(DelegationRibbon|| {}));
