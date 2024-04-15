b41a909cb1f6482bde0ead627757b460f58e2dfc
Zhen Yu Yin <106572740+dpuuqb@users.noreply.github.com>
2024-04-15 13:01:41 -0400

    Update EndDelegation.cs

--- DelegationPlugins/EndDelegation.cs
+++ DelegationPlugins/EndDelegation.cs
@@ -1,4 +1,4 @@
-﻿using DelegationPlugins.Entities;
+using DelegationPlugins.Entities;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using SharedLibrary;
@@ -11,12 +11,13 @@ using System.Threading.Tasks;
 
 namespace DelegationPlugins
 {
-    public class EndDelegation: JdxPlugin
+    public class EndDelegation : JdxPlugin
     {
-        public EndDelegation() {
+        public EndDelegation()
+        {
 
             RegisterEvent(PipelineStage.PostOperation, PipelineMessage.Update, Delegation.EntityName, ExcuteSingle);
-            RegisterEvent(PipelineStage.PostOperation, PipelineMessage.Delete, CustomTrigger.EntityName, ExecuteMultiple);
+            RegisterEvent(PipelineStage.PostOperation, PipelineMessage.Delete, Customtrigger.EntityName, ExecuteMultiple);
         }
 
         /// <summary>
@@ -25,30 +26,54 @@ namespace DelegationPlugins
         /// <param name="context"></param>
         private void ExcuteSingle(LocalPluginContext context)
         {
-            context.Trace($"Execute Single Process: delegation canceled manually.");
-            int pre_status = context.PreImage.GetAttributeValue<OptionSetValue>(Delegation.Status).Value;
-            int post_status = context.PostImage.GetAttributeValue<OptionSetValue>(Delegation.Status).Value;
-
-            if (pre_status == 0 && post_status == 1) 
+            
+            int pre_state = context.PreImage.GetAttributeValue<OptionSetValue>(Common.StateCode).Value;
+            int pre_status = context.PreImage.GetAttributeValue<OptionSetValue>(Common.StatusCode).Value;
+            int post_state = context.PostImage.GetAttributeValue<OptionSetValue>(Common.StateCode).Value;
+            int post_status = context.PreImage.GetAttributeValue<OptionSetValue>(Common.StatusCode).Value;
+            // From Delegating to Canceled or Expired
+            if (pre_state == 0 && post_state == 1 && pre_status == 952700002)
             {
+                context.Trace($"Execute Single Process: delegation canceled manually.");
                 Entity target = context.PluginExecutionContext.InputParameters["Target"] as Entity;
                 context.Trace($"Retrieved target: {target.Id}.");
                 Entity delegation = context.OrganizationService.Retrieve(Delegation.EntityName, target.Id, new ColumnSet(true));
                 context.Trace($"Retrieved delegation: {delegation.Id}.");
-               
-                DelegationUtils.EndDelegating(context, delegation);
-               
+
+                OrganizationRequestCollection organizationRequests = new OrganizationRequestCollection();
+                DelegationManager delegationManager = new DelegationManager(context);
+
+                organizationRequests.AddRange(delegationManager.CreateLeaveTeamRequests(delegation));
+                organizationRequests.AddRange(delegationManager.CreateEndDelegationReassignRequests(delegation));
+
+                delegationManager.ExcuteMultiple(organizationRequests);
             }
         }
-
+        /// <summary>
+        /// Handle system schedule jobs for delegation expiry event
+        /// </summary>
+        /// <param name="context"></param>
         private void ExecuteMultiple(LocalPluginContext context)
         {
             context.Trace($"Execute multiple Processes: delegations expired.");
             #region find all delegating delegations that expiry date are on execution date.
-            List<Entity> delegations = context.OrganizationDataContext.CreateQuery(Delegation.EntityName).Where(d => d.GetAttributeValue<DateTime>(Delegation.EffectiveDate).Equals(DateTime.Today.AddDays(1)) && d.GetAttributeValue<OptionSetValue>(Delegation.StatusReason).Value == 952700002).ToList();
+            List<Entity> delegations = context.OrganizationDataContext.CreateQuery(Delegation.EntityName)
+                .Where(d => d.GetAttributeValue<DateTime>(Delegation.Effectivedate).Equals(DateTime.Today.AddDays(1)) && d.GetAttributeValue<OptionSetValue>(Common.StatusCode).Value.Equals((int)Delegation.StatusCode_OptionSet.Delegating))
+                .ToList();
             #endregion
 
-            delegations.ForEach(delegation => DelegationUtils.EndDelegating(context, delegation));
+            DelegationManager delegationManager = new DelegationManager(context);
+            OrganizationRequestCollection requests = new OrganizationRequestCollection();
+
+
+            delegations.ForEach(delegation =>
+            {
+                requests.AddRange(delegationManager.CreateLeaveTeamRequests(delegation));
+                requests.AddRange(delegationManager.CreateEndDelegationReassignRequests(delegation));
+
+            });
+
+            delegationManager.ExcuteMultiple(requests);
         }
     }
 }
