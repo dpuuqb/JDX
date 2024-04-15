75621ac63a7e3e8a0317f6000e8464e3185da483
Zhen Yu Yin <106572740+dpuuqb@users.noreply.github.com>
2024-04-15 12:56:19 -0400

    Update CustomTrigger.cs
    update trigger

--- DelegationPlugins/Entities/CustomTrigger.cs
+++ DelegationPlugins/Entities/CustomTrigger.cs
@@ -1,26 +1,42 @@
 // *********************************************************************
-// Created by : Latebound Constants Generator 1.2023.12.1 for XrmToolBox
+// Created by : Latebound Constants Generator 1.2021.12.1 for XrmToolBox
 // Author     : Jonas Rapp https://jonasr.app/
 // GitHub     : https://github.com/rappen/LCG-UDG/
-// Source Org : https://orgb649ef22.crm3.dynamics.com
-// Filename   : C:\Users\User\source\repos\JDX\DelegationPlugins\Entities\CustomTrigger.cs
-// Created    : 2024-04-08 20:29:59
+// Source Org : https://dv-shl0.cdt.gccas-gccase.gc.ca/
+// Filename   : C:\Users\clh641\source\test lab\JDX\DelegationPlugins\Entities\CustomTrigger.cs
+// Created    : 2024-04-12 19:55:24
 // *********************************************************************
+
 namespace DelegationPlugins.Entities
 {
     /// <summary>DisplayName: Custom Trigger, OwnershipType: OrganizationOwned, IntroducedVersion: 1.0.0.0</summary>
-    public static class CustomTrigger
+    public static class Customtrigger
     {
-        public const string EntityName = "jms_trigger";
-        public const string EntityCollectionName = "jms_triggers";
+        public const string EntityName = "jms_customtrigger";
+        public const string EntityCollectionName = "jms_customtriggers";
 
-        #region Relationships
+        #region Attributes
 
-        /// <summary>Parent: "Organization" Child: "CustomTrigger" Lookup: "OrganizationId"</summary>
-        public const string RelM1_CustomTriggerOrganizationId = "organization_jms_trigger";
-        /// <summary>Parent: "CustomTrigger" Child: "FieldSharing" Lookup: ""</summary>
-        public const string Rel1M_jms_trigger_PrincipalObjectAttributeAccesses = "jms_trigger_PrincipalObjectAttributeAccesses";
+        /// <summary>Type: Uniqueidentifier, RequiredLevel: SystemRequired</summary>
+        public const string PrimaryKey = "jms_customtriggerid";
+        /// <summary>Type: String, RequiredLevel: ApplicationRequired, MaxLength: 100, Format: Text</summary>
+        public const string PrimaryName = "jms_name";
 
-        #endregion Relationships
+        #endregion Attributes
+
+        #region OptionSets
+
+        public enum StateCode_OptionSet
+        {
+            Active = 0,
+            Inactive = 1
+        }
+        public enum StatusCode_OptionSet
+        {
+            Active = 1,
+            Inactive = 2
+        }
+
+        #endregion OptionSets
     }
 }
